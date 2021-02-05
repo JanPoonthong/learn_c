010dd48068afeca8abc95d655369a39fd872f3ea
Jan Poonthong <janpoonthong628@gmail.com>
2021-02-05 15:35:54 +0700

    Declare a variable before scanf

--- caleb/area_circle.c
+++ caleb/area_circle.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int radius;
     printf("The a radius: ");
+    int radius;
     scanf("%i", &radius);
     double area = 3.14159 * (radius * radius);
     printf("The area of a circle with %i radius is %f\n", radius, area);
