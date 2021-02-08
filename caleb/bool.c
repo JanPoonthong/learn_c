d9d8e3a322492e699f64f974e0a6a31dfdfe2e61
Jan Poonthong <janpoonthong628@gmail.com>
2021-02-08 12:52:57 +0700

    Boolean

---
+++ caleb/bool.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    bool janIsFat = true;
+    printf("Is Jan Fat (1 is yes, 0 is no)? %i\n", janIsFat);
+    printf("%i\n", janIsFat + 10);
+    return 0;
+}
